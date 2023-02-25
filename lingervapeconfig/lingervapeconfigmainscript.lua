4e01104d870c8d4326a1004484c140156a0766ee
supercoolbro <121699538+wowletsgo@users.noreply.github.com>
2023-02-24 21:06:14 -0800

    Update lingervapeconfigmainscript.lua

--- lingervapeconfig/lingervapeconfigmainscript.lua
+++ lingervapeconfig/lingervapeconfigmainscript.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/wowletsgo/scripts/main/lingervapeconfigloader'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/wowletsgo/scripts/main/lingervapeconfig/lingervapeconfigloader(dont%20execute%20this).lua'),true))()
