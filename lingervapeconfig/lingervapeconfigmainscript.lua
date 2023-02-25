37dbed3925c6853767569ad9ebe7ae7dc90c5990
supercoolbro <121699538+wowletsgo@users.noreply.github.com>
2023-02-25 08:44:27 -0800

    Update lingervapeconfigmainscript.lua

--- lingervapeconfig/lingervapeconfigmainscript.lua
+++ lingervapeconfig/lingervapeconfigmainscript.lua
@@ -1,5 +1,3 @@
--- linger vape config
--- must have the config setup before using (in github)
--- make sure to follow the setup in the github before executing otherwise it will just execute ur normal vape
+
 
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/wowletsgo/scripts/main/lingervapeconfig/lingervapeconfigloader(dont%20execute%20this).lua'),true))()
