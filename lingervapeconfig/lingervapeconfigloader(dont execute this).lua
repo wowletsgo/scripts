b6fda20028d3c136f615d4bcb6d2a0db068e1a42
supercoolbro <121699538+wowletsgo@users.noreply.github.com>
2023-02-26 17:12:19 -0800

    Update lingervapeconfigloader(dont execute this).lua

--- lingervapeconfig/lingervapeconfigloader(dont execute this).lua
+++ lingervapeconfig/lingervapeconfigloader(dont execute this).lua
@@ -1,4 +1,6 @@
-game.Workspace.CurrentCamera.FieldOfView = 120
+while wait(0.000001) do
+   game.Workspace.CurrentCamera.FieldOfView = 120
+end  
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
 
