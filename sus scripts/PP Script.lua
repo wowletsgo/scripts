9ffe64dd62732bfa8c857f71e4ee9663d19d1799
supercoolbro <121699538+wowletsgo@users.noreply.github.com>
2023-03-02 19:49:13 -0800

    Update PP Script.lua

--- sus scripts/PP Script.lua
+++ sus scripts/PP Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wowletsgo/scripts/main/sus%20scripts/PP%20Script%20Source"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wowletsgo/scripts/main/sus%20scripts/PP%20Script%20Source.lua"))()
